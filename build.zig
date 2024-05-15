dcb559ff4a90e8cb982e43bc87254fed10ef005e
Ching Pei Yang <badnam3o.0@gmail.com>
2024-05-15 20:48:44 +0200

    build.zig: add deps

--- build.zig
+++ build.zig
@@ -37,6 +37,41 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
+    // GLAD
+    const glad = b.addStaticLibrary(.{
+        .name = "glad",
+        .target = target,
+        .optimize = optimize,
+    });
+    glad.linkLibC();
+    // glad.force_pic = true;
+    glad.addCSourceFile(.{
+        .file = .{ .cwd_relative = "glad/src/glad.c" },
+    });
+    glad.addIncludePath(.{ .cwd_relative = "glad/include" });
+
+    const glad_step = b.step("glad", "build glad");
+    glad_step.dependOn(&glad.step);
+
+    // 1. Triangle
+    const one = b.addExecutable(.{
+        .name = "hello_triangle",
+        .root_source_file = b.path("src/1_hello_triangle.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    one.linkLibrary(glad);
+    one.linkSystemLibrary("glfw");
+    one.linkSystemLibrary("gl");
+    one.linkLibC();
+    b.installArtifact(one);
+
+    const run_cmd1 = b.addRunArtifact(one);
+    run_cmd1.step.dependOn(b.getInstallStep());
+
+    const run_step1 = b.step("triangle", "Run hello triangle");
+    run_step1.dependOn(&run_cmd1.step);
+
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
